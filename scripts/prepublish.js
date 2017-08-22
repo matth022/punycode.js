bab087da83c0bd9e02eb66ba19dd0e00311b69cb
matth022 <matthieu.hertoghe@student.unamur.be>
2017-08-22 15:20:24 +0200

    Update prepublish.js

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+var fs = require('fs');
+var path = require('path');
 
-const regex = /module\.exports = punycode;/;
+var regex = /module\.exports = punycode;/;
 
-const sourceContents = fs.readFileSync(path.resolve(__dirname, '../punycode.js'), 'utf-8');
+var sourceContents = fs.readFileSync(path.resolve(__dirname, '../punycode.js'), 'utf-8');
 
 if (!regex.test(sourceContents)) {
 	throw new Error('The underlying library has changed. Please update the prepublish script.');
 }
 
-const outputContents = sourceContents.replace(regex, 'export default punycode;');
+var outputContents = sourceContents.replace(regex, 'export default punycode;');
 
 fs.writeFileSync(path.resolve(__dirname, '../punycode.es6.js'), outputContents);
